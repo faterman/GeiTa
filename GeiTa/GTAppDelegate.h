4b12a69e8206950b2766844f2c755a6080440b87
Mr-LiXiangYu <1198030047@qq.com>
2013-12-17 09:37:49 +0800

    添加网络状态监控

--- GeiTa/GTAppDelegate.h
+++ GeiTa/GTAppDelegate.h
@@ -7,9 +7,16 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "MBProgressHUD.h"
+#import "GTLocation.h"
 
 @interface GTAppDelegate : UIResponder <UIApplicationDelegate>
-
+{
+    GTMessageCenter* messageCenter;
+    GTUser* user;
+    MBProgressHUD* hud;
+}
 @property (strong, nonatomic) UIWindow *window;
+@property (nonatomic, assign) BOOL loadUserInfo;
 
 @end
--- GeiTa/GTAppDelegate.m
+++ GeiTa/GTAppDelegate.m
@@ -18,10 +18,26 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+#ifdef LSDEBUG
+    NSLog(@"\n%@\n",NSHomeDirectory());
+#endif
+    
+    messageCenter=[GTMessageCenter defaultCenter];
+    
+    [UIImage setIsRetina];//设置一个全局的屏幕变量
+    [GTSave setUserDefaults];//设置全局的用户信息存储变量
+    user=[GTUser currentUser];
+    [user copyPreUser:[GTSave obtainUser]];//获取上次存储的用户信息
+    [GTLocationManager start];//开始定位
+    
+    //启动网络监控
+    [self startInternetMonitor];
+    
     self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
@@ -50,6 +66,47 @@
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+#pragma mark- 启动网络监视
+- (void)startInternetMonitor
+{
+    //网络状态监控
+    [messageCenter addObserver:self selector:@selector(reachabilityChanged:) name:kReachabilityChangedNotification object:nil];
+    
+    //持续监视
+    //监视服务器连通状况
+    Reachability* hostReachability = [[Reachability reachabilityWithHostName:@"www.apple.com"] retain];
+    [hostReachability startNotifier];
+}
+
+#pragma mark- 网络状态的监视方法
+- (void)reachabilityChanged:(NSNotification*)notification//网络类型变化判断
+{
+    Reachability* reachability = notification.object;
+    NSParameterAssert([reachability isKindOfClass: [Reachability class]]);
+    switch ([reachability currentReachabilityStatus])
+    {
+        case NotReachable:
+            GTLOG(@"当前无网络连接,通知来源于:%@",notification.name);
+            break;
+        case ReachableViaWiFi:
+            GTLOG(@"当前连接WIFI,通知来源于:%@",notification.name);
+            break;
+        case ReachableViaWWAN:
+            GTLOG(@"当前连接3G,通知来源于:%@",notification.name);
+            break;
+        default:
+            GTLOG(@"当前有网络连接状态未知,通知来源于:%@",notification.name);
+    }
+    
+    //只有在之前状态或者当前状态有一个为不连接的情况才发出通知
+    NetworkStatus networkStatus=user.networkStatus;
+    user.networkStatus=[reachability currentReachabilityStatus];
+    if(networkStatus==NotReachable || [reachability currentReachabilityStatus]==NotReachable)
+    {
+        [messageCenter postNotificationName:LSNotificationNetworkStatusChanged object:nil];
+    }
 }
 
 @end
